8b98857d16a0d6c1255d1af52e320fcba825a5e3
ShakibRamez <shakibramiz1@icloud.com>
2025-06-04 10:55:19 +0430

    arrays

---
+++ array.js
@@ -0,0 +1,7 @@
+let supersMan = ['super man',  'batman', 'spiderman', 'aquaman', 1, "hello", true, false, ['good', 'evil', 'smile']];
+
+// delete supersMan[2];
+// supersMan[4] = 'wonder woman';
+// console.log(supersMan);
+
+console.log(supersMan[8][2]);

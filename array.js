fe03abba9c2fa744d43bde1132b1fed9c896bbfc
ShakibRamez <shakibramiz1@icloud.com>
2025-06-04 10:58:41 +0430

    task completed

--- array.js
+++ array.js
@@ -1,7 +1,16 @@
-let supersMan = ['super man',  'batman', 'spiderman', 'aquaman', 1, "hello", true, false, ['good', 'evil', 'smile']];
+// let supersMan = ['super man',  'batman', 'spiderman', 'aquaman', 1, "hello", true, false, ['good', 'evil', 'smile']];
 
-// delete supersMan[2];
-// supersMan[4] = 'wonder woman';
-// console.log(supersMan);
+// // delete supersMan[2];
+// // supersMan[4] = 'wonder woman';
+// // console.log(supersMan);
 
-console.log(supersMan[8][2]);
+// console.log(supersMan[8][2]);
+
+// task
+
+let countries = ['england', 'france', 'spain'];
+countries[3] = 'italy';
+console.log(countries);
+
+let colors = ['red', 'purple', 'green', ['yellow', 'blue', 'orange']];
+console.log(colors[3][2])
